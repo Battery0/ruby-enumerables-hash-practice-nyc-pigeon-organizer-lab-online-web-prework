344ca3351589fe402da84822b77a0ef25cba64fb
Ian King <kickthechair@gmail.com>
2021-05-03 15:47:31 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,11 +1,8 @@
 require 'pry'
 
 def nyc_pigeon_organizer(data)
-  
- 
-  
   data.each_with_object({}) do |(key, value), final_array|
-    a[k] = v + 3
+    final_array[k] = v + 3
   end
-   binding.pry
+  binding.pry
 end
