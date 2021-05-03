41705c93830112a8a2f1792938f4063ae91137fb
Ian King <kickthechair@gmail.com>
2021-05-03 15:46:57 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,7 +4,7 @@ def nyc_pigeon_organizer(data)
   
   binding.pry
   
-hash.each_with_object({}) do |(k, v), a|
- a[k] = v + 3
-end
+  data.each_with_object({}) do |(key, value), a|
+    a[k] = v + 3
+  end
 end
