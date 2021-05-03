dea9fdae392f406ffe65e3fcc0a731b60b3035cf
Ian King <kickthechair@gmail.com>
2021-05-03 16:12:17 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -9,7 +9,7 @@ def nyc_pigeon_organizer(data)
         end
         if !final_array[name][key]
           !final_array[name][key] = []
-        en
+        end
         final_array[name][key].push(inner_key.to_s)
       }
     }
